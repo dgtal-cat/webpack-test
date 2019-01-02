b398362df42d743729220377634e17e99469124e
Dgtal-Cat <dgtal.cat@gmail.com>
2019-01-02 15:06:30 +0300

    initial react app

--- src/components/app.js
+++ src/components/app.js
@@ -3,17 +3,11 @@ import React from "react";
 import '../styles/reset.css';
 import '../styles/app.css';
 
-import Form from "./form";
-import Info from "./info";
-import Weather from "./weather";
-
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Info />
-                <Form />
-                <Weather/>
+                <h1>hello</h1>
             </div>
         );
     }
